144ad509c4a49c46daed1241f88f007700761e32
Tariq <alskrantariq@gmail.com>
2025-01-13 15:28:09 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
 
     // Assentify
     implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("com.github.AssentifyLTD:Assentify.SDK.Android:Tag")
+    implementation("com.github.AssentifyLTD:Assentify.SDK.Android:TAG")
 
 
 
