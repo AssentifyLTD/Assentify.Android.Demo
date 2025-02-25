826893beae9e5318d4c5decb643f5689c037c08c
Tariq <alskrantariq@gmail.com>
2025-02-25 23:29:32 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
 
     // Assentify
     implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("com.github.AssentifyLTD:Assentify.SDK.Android:0.0.61")
+    implementation("com.github.AssentifyLTD:Assentify.SDK.Android:0.0.62")
 
 
 
--- app/src/main/java/com/example/androiddemoapp/FaceMatchActivity.kt
+++ app/src/main/java/com/example/androiddemoapp/FaceMatchActivity.kt
@@ -17,17 +17,17 @@ import com.assentify.sdk.FaceMatch.FaceResponseModel
 import com.assentify.sdk.Models.BaseResponseDataModel
 import com.assentify.sdk.ScanIDCard.IDCardCallback
 
-class FaceMatchActivity : AppCompatActivity() , FaceMatchCallback {
+class FaceMatchActivity : AppCompatActivity(), FaceMatchCallback {
     private lateinit var image: String;
-    private lateinit var base64Image:String;
-    private lateinit var infoText:TextView;
-    private lateinit var face:FaceMatch;
+    private lateinit var base64Image: String;
+    private lateinit var infoText: TextView;
+    private lateinit var face: FaceMatch;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
         image = intent.getStringExtra("image")!!
-         base64Image =
+        base64Image =
             ImageToBase64Converter().execute(image).get()
         Thread.sleep(1000)
         infoText = findViewById(R.id.infoTextTest)
@@ -40,78 +40,89 @@ class FaceMatchActivity : AppCompatActivity() , FaceMatchCallback {
         val assentifySdk: AssentifySdk = AssentifySdkObject.getAssentifySdkObject();
         face = assentifySdk.startFaceMatch(
             this@FaceMatchActivity, // This activity implemented from from FaceMatchCallback
-           base64Image , // Target  Image
+            base64Image, // Target  Image
             true // Show 3 - 2 - 1 Count Down before  start scanning
         );
-       var fragmentManager = supportFragmentManager
-       var transaction = fragmentManager.beginTransaction()
+        var fragmentManager = supportFragmentManager
+        var transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentContainer, face)
         transaction.addToBackStack(null) // Optional: Adds the transaction to the back stack
         transaction.commit()
     }
 
     override fun onCardDetected(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onClipPreparationComplete(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onComplete(dataModel: FaceResponseModel) {
-        Log.e("Events Here Face Page",dataModel.toString())
+        Log.e("Events Here Face Page", dataModel.toString())
         Log.e("Events Here Face Page", dataModel.faceExtractedModel!!.isLive!!.toString())
-        Log.e("Events Here Face Page",dataModel.faceExtractedModel!!.secondImageFace!!)
-        Log.e("Events Here Face Page",dataModel.faceExtractedModel!!.baseImageFace!!)
+        Log.e("Events Here Face Page", dataModel.faceExtractedModel!!.secondImageFace!!)
+        Log.e("Events Here Face Page", dataModel.faceExtractedModel!!.baseImageFace!!)
         Log.e("Events Here Face Page", dataModel.faceExtractedModel!!.percentageMatch!!.toString())
-       // var faceExtractedModel = FaceExtractedModel.fromJson(dataModel.response);
-        runOnUiThread{
+        // var faceExtractedModel = FaceExtractedModel.fromJson(dataModel.response);
+        runOnUiThread {
             infoText.visibility = View.VISIBLE
         }
 
         face.stopScanning();
         val intent = Intent(this, ResultActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        intent.putExtra("baseImageFace",dataModel.faceExtractedModel!!.baseImageFace)
-        intent.putExtra("secondImageFace",dataModel.faceExtractedModel!!.secondImageFace)
-        intent.putExtra("percentageMatch",dataModel.faceExtractedModel!!.percentageMatch)
-        intent.putExtra("isLive",dataModel.faceExtractedModel!!.isLive)
+        intent.putExtra("baseImageFace", dataModel.faceExtractedModel!!.baseImageFace)
+        intent.putExtra("secondImageFace", dataModel.faceExtractedModel!!.secondImageFace)
+        intent.putExtra("percentageMatch", dataModel.faceExtractedModel!!.percentageMatch)
+        intent.putExtra("isLive", dataModel.faceExtractedModel!!.isLive)
         startActivity(intent)
         finish();
 
-       dataModel.faceExtractedModel!!.outputProperties?.let { OutputPropertiesModelFace.setOutputPropertiesModel(it) };
+        dataModel.faceExtractedModel!!.outputProperties?.let {
+            OutputPropertiesModelFace.setOutputPropertiesModel(
+                it
+            )
+        };
 
 
     }
 
     override fun onDocumentCaptured(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onDocumentCropped(dataModel: BaseResponseDataModel) {
-        
+
     }
 
-    override fun onEnvironmentalConditionsChange(brightnessEvents: BrightnessEvents,
-                                                 motion: MotionType,
-                                                 faceEvents: FaceEvents,
-                                                 zoomType: ZoomType) {
-        runOnUiThread{
+    override fun onEnvironmentalConditionsChange(
+        brightnessEvents: BrightnessEvents,
+        motion: MotionType,
+        faceEvents: FaceEvents,
+        zoomType: ZoomType
+    ) {
+        runOnUiThread {
             infoText.visibility = View.VISIBLE
+            infoText.text = "brightnessEvents ${brightnessEvents.toString()}\n" +
+                    "motion ${motion.toString()}\n" +
+                    "faceEvents ${faceEvents.toString()}\n" +
+                    "zoomType ${zoomType.toString()}\n"
+
         }
     }
 
 
     override fun onError(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onFaceDetected(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onFaceExtracted(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onLivenessUpdate(dataModel: BaseResponseDataModel) {
@@ -119,43 +130,43 @@ class FaceMatchActivity : AppCompatActivity() , FaceMatchCallback {
     }
 
     override fun onMrzDetected(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onMrzExtracted(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onNoFaceDetected(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onNoMrzDetected(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onQualityCheckAvailable(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onRetry(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onSend() {
-        Log.e("Events Here Face Page","onSend")
+        Log.e("Events Here Face Page", "onSend")
     }
 
     override fun onStatusUpdated(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onUpdated(dataModel: BaseResponseDataModel) {
-        
+
     }
 
     override fun onUploadFailed(dataModel: BaseResponseDataModel) {
-        
+
     }
 
 }
